175931ed57076b59f1a9cd60731102c457756ba2
Georgi Fuchedzhiev <georgifuchedzhiev@infinitelambda.com>
2022-02-04 09:31:06 +0200

    Created the repository and pushed the image.

---
+++ terraform_assessment/main.tf
@@ -0,0 +1,68 @@
+#ECR creation:
+resource "aws_ecr_repository" "tap_gf_repo" {
+  name = "tap_gf_repo"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_ecr_authorization_token" "aws_token"{
+  registry_id = aws_ecr_repository.tap_gf_repo.registry_id
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+  registry_auth {
+    address = data.aws_ecr_authorization_token.aws_token.proxy_endpoint
+    username = data.aws_ecr_authorization_token.aws_token.user_name
+    password = data.aws_ecr_authorization_token.aws_token.password
+
+  }
+}
+
+resource "docker_registry_image" "tap_gf_connection_app" {
+  name = "${aws_ecr_repository.tap_gf_repo.repository_url}"
+  build {
+    context = path.cwd
+    dockerfile = "Dockerfile"
+  }
+}
+
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_ecr_repository_policy" "tap_gf_ecr_policy" {
+  repository = aws_ecr_repository.tap_gf_repo.name
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPushPull",
+            "Effect": "Allow",
+            "Principal": {
+              "AWS" : [
+                "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
+                  ]
+            },
+            "Action": [
+              "ecr:BatchGetImage",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:CompleteLayerUpload",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:InitiateLayerUpload",
+              "ecr:PutImage",
+              "ecr:UploadLayerPart"
+            ]
+        }
+    ]
+}
+EOF
+}
